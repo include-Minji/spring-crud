05a822a722bb7e9fa0f4333a4a4feb71dc904bdf
cloud <the_1975@naver.com>
2021-11-22 21:04:42 +0900

    C++ 과제

---
+++ git/211119.hpp
@@ -0,0 +1,19 @@
+
+#ifndef _11119_hpp
+#define _11119_hpp
+
+#include <stdio.h>
+
+#endif /* _11119_hpp */
+
+
+class Num {
+    
+public:
+    
+    int num;
+    void reset() { num = 0;}
+    
+};
+
+
